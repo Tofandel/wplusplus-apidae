de7069cb55e45b917a98371915ebff48a7d68db0
Tofandel <adrien.foulon.lfgeb@gmail.com>
2018-07-16 09:33:09 +0200

    Wrong template dir

--- WPlusPlusApidae.php
+++ WPlusPlusApidae.php
@@ -22,7 +22,7 @@ if ( ! class_exists( 'Tofandel\WPlusPlusCore' ) ) {
  * Plugin Name: W++ Apidae
  * Plugin URI: https://github.com/Tofandel/wplusplus-apidae/
  * Description: W++ apidae allows you to use apidae with worpress simply by creating Twig templates
- * Version: 1.2
+ * Version: 1.2.1
  * Author: Adrien Foulon <tofandel@tukan.hu>
  * Author URI: https://tukan.fr/a-propos/#adrien-foulon
  * Text Domain: wplusplusapidae
