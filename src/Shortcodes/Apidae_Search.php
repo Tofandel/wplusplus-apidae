d3cc57e7f3d3f5f94562c51481c83d2c24a072a7
Tofandel <adrien.foulon.lfgeb@gmail.com>
2018-08-07 10:47:07 +0200

    Complete Search Functionnalities

--- src/Shortcodes/Apidae_Search.php
+++ src/Shortcodes/Apidae_Search.php
@@ -51,6 +51,37 @@ class Apidae_Search implements WP_Shortcode {
 					'param_name'  => 'date_inputs',
 					'std'         => 'true',
 					'admin_label' => true,
+				),
+				array(
+					'type'       => 'text',
+					'heading'    => esc_html__( 'Start Date Placeholder', $WPlusPlusApidae->getTextDomain() ),
+					'param_name' => 'start_placeholder',
+					'std'        => __( 'Start date', $WPlusPlusApidae->getTextDomain() ),
+				),
+				array(
+					'type'       => 'text',
+					'heading'    => esc_html__( 'End Date Placeholder', $WPlusPlusApidae->getTextDomain() ),
+					'param_name' => 'end_placeholder',
+					'std'        => __( 'End date', $WPlusPlusApidae->getTextDomain() ),
+				),
+				array(
+					'type'       => 'text',
+					'heading'    => esc_html__( 'Search Placeholder', $WPlusPlusApidae->getTextDomain() ),
+					'param_name' => 'search_placeholder',
+					'std'        => __( 'Search...', $WPlusPlusApidae->getTextDomain() ),
+				),
+				array(
+					'type'       => 'text',
+					'heading'    => esc_html__( 'Submit Title', $WPlusPlusApidae->getTextDomain() ),
+					'param_name' => 'submit_title',
+					'std'        => __( 'Search', $WPlusPlusApidae->getTextDomain() ),
+				),
+				array(
+					'type'        => 'textarea_raw_html',
+					'heading'     => esc_html__( 'Submit Text', $WPlusPlusApidae->getTextDomain() ),
+					'description' => esc_html__( 'Leave it empty to display a SVG search icon', $WPlusPlusApidae->getTextDomain() ),
+					'param_name'  => 'submit_text',
+					'std'         => '',
 				)
 			)
 		);
@@ -71,9 +102,10 @@ class Apidae_Search implements WP_Shortcode {
 		$page_query = array();
 
 
-		$search    = get_query_var( 'apisearch', '' );
-		$dateDebut = get_query_var( 'datedebut', '' );
-		$dateFin   = get_query_var( 'datefin', '' );
+		$search           = get_query_var( 'apisearch', '' );
+		$dateDebut        = get_query_var( 'datedebut', '' );
+		$dateFin          = get_query_var( 'datefin', '' );
+		$searchCategories = get_query_var( 'apicategories', '' );
 
 		if ( ! empty( $dateDebut ) && ! Apidae_List::checkDateFormat( $dateDebut ) ) {
 			$dateDebut = '';
@@ -89,6 +121,14 @@ class Apidae_Search implements WP_Shortcode {
 			}
 		}
 
+		$submit_text = ! empty( $atts['submit_text'] ) ? strip_tags( $atts['submit_text'], array(
+			'span',
+			'i',
+			'p',
+			'b',
+			'strong'
+		) ) : '<svg width="32" height="32" viewBox="0 0 32 32" class="search-svg"><style type="text/css">.se0{fill:none;stroke:#000000;stroke-width:2;stroke-linecap:round;}</style><circle class="se0" cx="13" cy="13" r="7"/><line class="se0" x1="18" x2="24" y1="18" y2="24"/></svg>';
+
 		$search_input = $atts['search_input'] == 'true';
 		$date_inputs  = $atts['date_inputs'] == 'true';
 
@@ -98,13 +138,13 @@ class Apidae_Search implements WP_Shortcode {
 		if ( $date_inputs ) {
 			wp_enqueue_script( 'jquery-ui-datepicker' );
 			$WPlusPlusApidae->addStyle( 'datepicker' );
-			$startPlaceholder = ! empty( $atts['start_placeholder'] ) ? $atts['start_placeholder'] : __( 'Start date', $WPlusPlusApidae->getTextDomain() );
-			$endPlaceholder   = ! empty( $atts['end_placeholder'] ) ? $atts['end_placeholder'] : __( 'End date', $WPlusPlusApidae->getTextDomain() );
+			$startPlaceholder = ! empty( $atts['start_placeholder'] ) ? esc_attr( $atts['start_placeholder'] ) : esc_attr__( 'Start date', $WPlusPlusApidae->getTextDomain() );
+			$endPlaceholder   = ! empty( $atts['end_placeholder'] ) ? esc_attr( $atts['end_placeholder'] ) : esc_attr__( 'End date', $WPlusPlusApidae->getTextDomain() );
 			$hasDates         = 'has-dates';
 		}
 		if ( $search_input ) {
-			$searchPlaceholder = ! empty( $atts['search_placeholder'] ) ? $atts['search_placeholder'] : __( 'Search...', $WPlusPlusApidae->getTextDomain() );
-			$submitTitle       = ! empty( $atts['submit_title'] ) ? $atts['submit_title'] : __( 'Search', $WPlusPlusApidae->getTextDomain() );
+			$searchPlaceholder = ! empty( $atts['search_placeholder'] ) ? esc_attr( $atts['search_placeholder'] ) : esc_attr__( 'Search...', $WPlusPlusApidae->getTextDomain() );
+			$submitTitle       = ! empty( $atts['submit_title'] ) ? esc_attr( $atts['submit_title'] ) : esc_attr__( 'Search', $WPlusPlusApidae->getTextDomain() );
 			$hasSearch         = 'has-search';
 		}
 
@@ -123,12 +163,15 @@ HTML;
 		$search_input = $search_input ? 'search' : 'hidden';
 		$date_inputs  = $date_inputs ? 'date' : 'hidden';
 
+		$searchCategories = ! empty( $searchCategories ) ? '<input type="hidden" name="apicategories" value="' . esc_attr( $searchCategories ) . '">' : '';
+
 		$html .= <<<HTML
 <form action="{$url}" class="apidae-searchform {$hasDates} {$hasSearch}" method="get">
+	{$searchCategories}
 	<input type="{$date_inputs}" name="datedebut" class="date" value="{$dateDebut}" placeholder="{$startPlaceholder}">
 	<input type="{$date_inputs}" name="datefin" class="date" value="{$dateFin}" placeholder="$endPlaceholder">
 	<input type="{$search_input}" name="apisearch" value="{$search}" placeholder="{$searchPlaceholder}" class="searchinput">
-	<button type="submit" title="{$submitTitle}" class="search-submit button"><svg width="32" height="32" viewBox="0 0 32 32" class="search-svg"><style type="text/css">.se0{fill:none;stroke:#000000;stroke-width:2;stroke-linecap:round;}</style><circle class="se0" cx="13" cy="13" r="7"/><line class="se0" x1="18" x2="24" y1="18" y2="24"/></svg></button>
+	<button type="submit" title="{$submitTitle}" class="search-submit button">{$submit_text}</button>
 </form>
 HTML;
 
