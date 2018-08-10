9dd3cbddeb55f5cf0faf87f146b0c891e953669b
Tofandel <adrien.foulon.lfgeb@gmail.com>
2018-08-10 13:45:41 +0200

    Copyright

--- WPlusPlusApidae.php
+++ WPlusPlusApidae.php
@@ -39,7 +39,7 @@ if ( ! class_exists( 'Tofandel\WPlusPlusCore' ) ) {
  * Plugin Name: W++ Apidae
  * Plugin URI: https://github.com/Tofandel/wplusplus-apidae/
  * Description: W++ apidae allows you to use apidae with wordpress simply by creating Twig templates
- * Version: 1.6
+ * Version: 1.6.1
  * Author: Adrien Foulon <tofandel@tukan.hu>
  * Author URI: https://tukan.fr/a-propos/#adrien-foulon
  * Text Domain: wppa
