de4ba041a9ee5af7e1ec2d0929901a90172530c7
Tofandel <adrien.foulon.lfgeb@gmail.com>
2018-08-10 13:32:27 +0200

    Copyright

--- WPlusPlusApidae.php
+++ WPlusPlusApidae.php
@@ -1,13 +1,7 @@
 <?php
 /**
- * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
+ * Copyright (c) Adrien Foulon - 2018.
  */
-
-
 namespace Tofandel;
 
 use Tofandel\Apidae\Modules\TemplateFilesHandler;
